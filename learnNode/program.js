43c305363caed78ecd629ef7080344a0ab890de6
jairorjuela <genius85862@gmail.com>
2019-04-12 22:03:04 -0500

    Baby Steps

--- learnNode/program.js
+++ learnNode/program.js
@@ -1,1 +1,10 @@
-console.log("HELLO WORLD");
+var length = process.argv.length
+var sum = 0
+
+for(var i = 2; i < length; i++){
+  var str = process.argv[i]
+  var num = Number(str)
+  sum = sum + num
+}
+
+console.log(sum);
